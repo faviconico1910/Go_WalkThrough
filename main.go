64ab864a523be9d92a9b1d8223fb9b64ca85e723
faviconico1910 <dauducanphu1910@gmail.com>
2026-06-12 15:07:31 +0700

    feat: print systeminfo as Otel Payload

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -23,12 +24,64 @@ type Config struct {
 	} `yaml:"collectors"`
 }
 
+type MetricDataPoint struct {
+	Timestamp string  `json:"timestamp"`
+	Value     float64 `json:"value"`
+}
+
+type Metric struct {
+	Name       string            `json:"name"`
+	Unit       string            `json:"unit"`
+	DataPoints []MetricDataPoint `json:"data_points"`
+}
+
+// định nghĩa otel payload
+type OtelPayload struct {
+	Resource struct {
+		ServiceName string `json:"service.name"`
+	} `json:"resource"`
+
+	Metrics []Metric `json:"metrics"`
+}
+
 type SystemInfo struct {
-	Name     string `json:"server_name"`
-	CPU      string `json:"cpu_usage"`
-	RAM      string `json:"ram_usage"`
-	Time     string `json:"timestamp"`
-	ErrorMsg string `json:"error_msg"`
+	Name     string  `json:"server_name"`
+	CPU      float64 `json:"cpu_usage"`
+	RAM      float64 `json:"ram_usage"`
+	Time     string  `json:"timestamp"`
+	ErrorMsg string  `json:"error_msg"`
+}
+
+// convert SystemInfo to OtelPayload
+
+func buildOtelPayload(sysInfo SystemInfo) OtelPayload {
+	var payload OtelPayload
+	payload.Resource.ServiceName = sysInfo.Name
+
+	payload.Metrics = []Metric{
+		{
+			Name: "cpu_usage",
+			Unit: "%",
+			DataPoints: []MetricDataPoint{
+				{
+					Value:     sysInfo.CPU,
+					Timestamp: sysInfo.Time,
+				},
+			},
+		},
+
+		{
+			Name: "ram_usage",
+			Unit: "%",
+			DataPoints: []MetricDataPoint{
+				{
+					Value:     sysInfo.RAM,
+					Timestamp: sysInfo.Time,
+				},
+			},
+		},
+	}
+	return payload
 }
 
 func getSystemInfo(ch chan<- SystemInfo, config Config) {
@@ -63,8 +116,8 @@ func getSystemInfo(ch chan<- SystemInfo, config Config) {
 
 		sysInfo := SystemInfo{
 			Name: config.Agent.Name,
-			CPU:  fmt.Sprintf("%.1f%%", cpuPercent[0]),
-			RAM:  fmt.Sprintf("%.1f%%", ramUsage.UsedPercent),
+			CPU:  cpuPercent[0],
+			RAM:  ramUsage.UsedPercent,
 			Time: time.Now().Format(time.RFC3339),
 		}
 		ch <- sysInfo // gửi dữ liệu vào channel
@@ -98,13 +151,22 @@ func main() {
 			continue
 		}
 
-		fmt.Printf(
-			"[INFO] System Info:\nServer Name: %s\nCPU Usage: %s\nRAM Usage: %s\nTimestamp: %s\n",
-			sysMetrics.Name,
-			sysMetrics.CPU,
-			sysMetrics.RAM,
-			sysMetrics.Time,
-		)
+		payload := buildOtelPayload(sysMetrics)
+		jsonBytes, err := json.MarshalIndent(payload, "", "  ")
+		if err != nil {
+			fmt.Printf("[ERROR]: Error marshaling payload: %v\n", err)
+			continue
+		}
+
+		fmt.Println(string(jsonBytes))
+
+		// fmt.Printf(
+		// 	"[INFO] System Info:\nServer Name: %s\nCPU Usage: %s\nRAM Usage: %s\nTimestamp: %s\n",
+		// 	sysMetrics.Name,
+		// 	sysMetrics.CPU,
+		// 	sysMetrics.RAM,
+		// 	sysMetrics.Time,
+		// )
 	}
 
 }
